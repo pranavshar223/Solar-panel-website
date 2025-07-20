d28da189515fcc4ba7b2feb7679dc898e022bfda
pranavshar223 <itsmepranav223@gmail.com>
2025-07-20 20:26:02 +0530

    Intial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const toggleBtn = document.getElementById("nav-links")
+const nevlinks =document.getElementById("menu-toggle")
+
+
+toggleBtn.addEventListener('click', () =>{
+    nevlinks.classList,toggle('show');
+} );
